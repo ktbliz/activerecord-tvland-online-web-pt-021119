cdc66bb228877e0f49fde989c4ec3e83c8d5eda8
Katie Blizzard <kblizzard11@gmail.com>
2019-05-19 23:58:32 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,11 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+  
+  def full_name 
+    first = self.first_name 
+    last = self.last_name 
+    "#{first} #{last}"
+  end 
   
 end
