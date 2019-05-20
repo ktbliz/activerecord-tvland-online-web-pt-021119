b3ca227423436422121f812f5990b575de0f674d
Katie Blizzard <kblizzard11@gmail.com>
2019-05-20 00:54:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters  
   has_many :characters
   
-  def build_network
-    
+  def build_network(hash)
+    new_network = Network.new(hash)
+    self.network - new_network 
   end 
   
 end
